10c7c96379680f8f957208c639f70a89a62fc051
Jerry <jerryluisbuntaran@gmail.com>
2017-08-11 13:56:34 +0700

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	app/src/main/java/com/example/rjtimothy/test123/MainActivity.java

--- app/src/main/java/com/example/rjtimothy/test123/MainActivity.java
+++ app/src/main/java/com/example/rjtimothy/test123/MainActivity.java
@@ -13,6 +13,10 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        if(gitu_jer == "asdasdasdadsadsad"){
+
+        }
+
 
     }
 }
